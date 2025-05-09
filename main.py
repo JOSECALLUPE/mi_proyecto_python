c72a7795c6ea0faf5872bdb6e4f37265b675b218
JOSECALLUPE <1515995@senati.pe>
2025-05-09 16:20:28 -0500

    Hice el programa interactivo que suma dos números ingresados

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # main.py
 print("Hola, este es mi primer archivo Python subido a GitHub desde CMD y Git Bash.")
+ 
+ # main.py
+def suma(a, b):
+    return a + b
+
+# Solicita datos al usuario
+x = int(input("Ingresa el primer número: "))
+y = int(input("Ingresa el segundo número: "))
+
+# Muestra la suma
+print("Suma:", suma(x, y))
